9f02756f8fbad31a3e3c3074748bbffa4146beb9
Eli Rubin-Calvert <elirc3@gmail.com>
2020-01-03 19:25:59 -0600

    added aliens

---
+++ QuickStart.java
@@ -0,0 +1,17 @@
+
+
+public class QuickStart {
+
+    public static void main(String[] args) {
+
+       
+
+        boolean isThisAnAlien = false;
+        if (isThisAnAlien == false) {
+            System.out.println("I aint scared of no alien");
+            System.out.println("no sir");
+
+        }
+    }
+}
+
