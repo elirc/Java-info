5142b946535b5a1bffb8df4fb65072f1795aee23
Eli Rubin-Calvert <elirc3@gmail.com>
2020-01-06 14:03:45 -0600

    for loops

--- exerciseSpeed.java
+++ exerciseSpeed.java
@@ -1,8 +1,14 @@
 public class exerciseSpeed{
-public static void main(String[] args){
-double gallons;
-double liters;
-gallons = 10;
-liters = gallons * 3.7854;
-System.out.println(gallons + " gallons is " + liters + "liters. ");
-}}
+public static void main(String[] args)
+{
+    double gallons;
+    double liters;
+    gallons = 10;
+    liters = gallons * 3.7854;
+    System.out.println(gallons + " gallons is " + liters + "liters. ");
+    }
+ 
+    {int count;
+for (count = 0; count < 5; count = count+1)
+System.out.println("this is a count" + count);
+System.out.println("donezo");}}
