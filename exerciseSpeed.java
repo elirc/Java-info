f4049a3a3474eb667727ef91d63898e7e262b637
Eli Rubin-Calvert <elirc3@gmail.com>
2020-01-07 14:45:37 -0600

    added speed,run

--- exerciseSpeed.java
+++ exerciseSpeed.java
@@ -1,14 +1,9 @@
 public class exerciseSpeed{
 public static void main(String[] args)
-{
-    double gallons;
-    double liters;
-    gallons = 10;
-    liters = gallons * 3.7854;
-    System.out.println(gallons + " gallons is " + liters + "liters. ");
-    }
+
  
     {int count;
-for (count = 0; count < 5; count = count+1)
+for (count = 0; count < 5; count++)
 System.out.println("this is a count" + count);
-System.out.println("donezo");}}
+System.out.println("donezo");
+system.out.println("done ");}}
